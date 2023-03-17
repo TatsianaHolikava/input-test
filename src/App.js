b2b171e2c912a1fd8fac320731e03140644ec59f
TatsiH <gtaneka@gmail.com>
2023-03-16 21:46:46 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -10,72 +10,58 @@ function App() {
     const [period, setPeriod] = useState('');
     const [form, setForm] = useState('');
     const [demoYear, setDemoYear] = useState("");
+    
 
-    console.log(form)
 
     const handlePeriodChange = (e) => {
         const value = e.target.value;
         setPeriod(value);
     };
 
+// const handleValidation = enteredValue => {
+//   const newRegExp = new RegExp('^([NA]|[0-9][1-9]?)$');
+//   if (newRegExp.test(enteredValue)) {
+//     setDemoYear(enteredValue);
+// }
+// }
 
-    const handleChangeYear = (e) => {
-        let value = e.target.value;
-        if (form === "64A") {
-            if (typeof value === "string" && value.length >=2) {
-                value = 'NA';
-            }
-                if (/^\d{1,2}$/.test(value) && value >= 1 && value <= 99) {
-                    if (value.length <= 1) {
-                        value = "0" + value;
-                    }
-                }
-        }
-        if (form === "64B" || form === "64C") {
-            if (typeof value === "string" && isNaN(value)) {
-                value = '';
-            }
-            if (/^\d{1,2}$/.test(value) && value >= 1 && value <= 99) {
-                if (value.length <= 1) {
-                    value = "0" + value;
-                }
-            }
-        }
-        setDemoYear(value);
-    };
+// const handleInputDemoYear = demonstrationYear => {
+// setState({demonstrationYear, wasAddFormPermormed: false})
+// }
 
 
-    // const handleChangeYear = (event) => {
-    //     let demoYear = event.target.value;
-    //     if (form === "64A") {
-    //         if (demoYear === "NA") {
-    //             setDemoYear(demoYear);
-    //         }else if(Number(demoYear) >= 1 && Number(demoYear) <= 9){
-    //             setDemoYear(`0${demoYear}`);
-    //         }
-    //         else {
-    //             setDemoYear("");
-    //         }
-    //     } else if (form === "64B" || form === "64C") {
-    //         if (Number(demoYear) >= 1 && Number(demoYear) <= 9) {
-    //             setDemoYear(`0${demoYear}`);
-    //         } else {
-    //             setDemoYear("");
-    //         }
-    //     }
-    // };
+    const handleChange = (e) => {
+      let value = e.target.value;
+      if (form === "64A") {
+  
+          if (/^\d{1,2}$/.test(value) && value >= 1 && value <= 99) {
+              if (value.length === 1) {
+                  value = "0" + value;
+              }
+          }
+  
+          if (typeof value === "string" && value.length === 1) {
+              value = 'NA';
+          }
+  
+          if (demoYear === "NA") {
+              value = '';
+              setDemoYear('');
+          }
+      }
+      if (form === "64B" || form === "64C") {
+          if (typeof value === "string" && isNaN(value)) {
+              value = '';
+          }
+          if (/^\d{1,2}$/.test(value) && value >= 1 && value <= 99) {
+              if (value.length <= 1) {
+                  value = "0" + value;
+              }
+          }
+      }
+      setDemoYear(value);
+  };
 
-
-    // if (form !== "64A") {
-    //     // Проверяем, что введено число и оно находится в диапазоне от 1 до 99
-    //     if (/^\d{1,2}$/.test(value) && value >= 1 && value <= 99) {
-    //         // Если введено однозначное число, добавляем перед ним 0
-    //         if (value.length <= 1) {
-    //             value = "0" + value;
-    //             setDemoYear(value);
-    //         }
-    //     }
-    // }
 
     return (
         <div className="App">1
@@ -157,31 +143,15 @@ function App() {
                     aria-label="Demo Year"
                     maxLength="2"
                     aria-describedby="inputGroup-sizing-default"
-                    onChange={handleChangeYear}
+                    onChange={handleChange}
                     value={demoYear}
                 />
-
             </InputGroup>
             }
-            {/* {period === "current period" && <InputGroup className="mb-3">
-       <InputGroup.Text id="inputGroup-sizing-default">
-          Demo Year
-        </InputGroup.Text>
-        <Form.Control
-          type="text"
-          aria-label="Demo Year"
-          maxLength="2"
-          aria-describedby="inputGroup-sizing-default"
-          onChange={handleChange}
-          value={demoYear}
-        />
-      </InputGroup>
-
-      } */}
-
         </div>
     );
 }
+
 
 export default App;
 
